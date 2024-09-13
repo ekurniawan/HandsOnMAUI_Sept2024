c3853382c95b42488b96eac6c6d66c2ffee23850
Erick Kurniawan <erick.kurniawan@gmail.com>
2024-09-13 15:03:19 +0700

    Add project files.

---
+++ Phoneworld/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Phoneworld
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Phoneworld/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Phoneworld
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void btnHello_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Hello", "Welcome to MAUI", "OK");
+        }
+
+        private void btnSubmit_Clicked(object sender, EventArgs e)
+        {
+            string username = entryUsername.Text;
+            DisplayAlert("Welcome", $"Hello {username}", "OK");
+        }
+    }
+
+}
